614651a2b3facfc4111891f4e74be5173bd6e2b1
geanbrandao <karlos867gean@gmail.com>
2021-04-05 16:46:23 -0300

    first unit tests

--- app/src/test/java/com/example/autobot/mvp/SignUpPresenterTest.kt
+++ app/src/test/java/com/example/autobot/mvp/SignUpPresenterTest.kt
@@ -1,8 +1,17 @@
 package com.example.autobot.mvp
 
+import com.example.autobot.extensions.isValidName
+import com.example.autobot.ui.signup.SignupFragment.Companion.ERROR_MESSAGE_FIRST_NAME
+import com.example.autobot.ui.signup.SignupFragment.Companion.ERROR_MESSAGE_NEED_TWO_NAMES
+import com.example.autobot.ui.signup.SignupFragment.Companion.ERROR_MESSAGE_REQUIRED_FIELD
+import com.example.autobot.ui.signup.SignupFragment.Companion.ERROR_MESSAGE_SECOND_NAME
+import com.example.autobot.ui.signup.SignupFragment.Companion.NO_ERROR_MESSAGE
 import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.Assert.*
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
@@ -23,5 +32,50 @@ class SignUpPresenterTest {
         presenter.isValid()
 
         verify(view).displaySuccessToast()
+    }
+
+    @Test
+    fun `is valid return when name is empty`() {
+        val name = ""
+        val errorMessage = name.isValidName()
+        val isValid = errorMessage.isEmpty()
+        assertThat(errorMessage, `is`(equalTo(ERROR_MESSAGE_REQUIRED_FIELD)))
+        assertThat(isValid, `is`(equalTo(false)))
+    }
+
+    @Test
+    fun `is valid return when dont has name and lastname`() {
+        val name = "Gean"
+        val errorMessage = name.isValidName()
+        val isValid = errorMessage.isEmpty()
+        assertThat(errorMessage, `is`(equalTo(ERROR_MESSAGE_NEED_TWO_NAMES)))
+        assertThat(isValid, `is`(equalTo(false)))
+    }
+
+    @Test
+    fun `is valid return when first name lenght is smaller than three`() {
+        val name = "Ge Carlos"
+        val errorMessage = name.isValidName()
+        val isValid = errorMessage.isEmpty()
+        assertThat(errorMessage, `is`(equalTo(ERROR_MESSAGE_FIRST_NAME)))
+        assertThat(isValid, `is`(equalTo(false)))
+    }
+
+    @Test
+    fun `is valid return when second name lenght is smaller than three`() {
+        val name = "Gean Ca"
+        val errorMessage = name.isValidName()
+        val isValid = errorMessage.isEmpty()
+        assertThat(errorMessage, `is`(equalTo(ERROR_MESSAGE_SECOND_NAME)))
+        assertThat(isValid, `is`(equalTo(false)))
+    }
+
+    @Test
+    fun `is valid return when is valid name`() {
+        val name = "Gean Carlos"
+        val errorMessage = name.isValidName()
+        val isValid = errorMessage.isEmpty()
+        assertThat(errorMessage, `is`(equalTo(NO_ERROR_MESSAGE)))
+        assertThat(isValid, `is`(equalTo(true)))
     }
 }
