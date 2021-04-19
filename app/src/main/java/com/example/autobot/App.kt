f33e09928507ecfbfc45d099a7d9b9e48d720dba
geanbrandao <karlos867gean@gmail.com>
2021-04-19 14:55:35 -0300

    add fastlane with AppCenter integration

--- app/src/main/java/com/example/autobot/App.kt
+++ app/src/main/java/com/example/autobot/App.kt
@@ -1,13 +1,21 @@
 package com.example.autobot
 
-import android.app.Application
 import androidx.multidex.MultiDexApplication
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import timber.log.Timber
+
 
 class App: MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+        AppCenter.start(
+            this, "67d73bc8-f5dd-4224-9003-6b0f5fc816b4",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
